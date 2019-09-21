9fed144766ddbe5671be4fadce4681454871a0f2
Gywlssww <tarahjjeon96@gmail.com>
2019-09-21 18:18:10 +0900

    가게 등록 시 중복 제거 안됌

--- ZeropayTest/app/src/main/java/com/example/zeropaytest/DBHelper.kt
+++ ZeropayTest/app/src/main/java/com/example/zeropaytest/DBHelper.kt
@@ -1,4 +1,4 @@
-package com.example.zeropaytest
+package com.example.todayzero.db
 
 import android.content.ContentValues
 import android.content.Context
@@ -6,10 +6,14 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 import android.util.Log
+import com.example.zeropaytest.deal
+import com.example.zeropaytest.store
+import com.example.zeropaytest.user
 
 class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION){
 
 
+    val zone= mutableMapOf<Int,String>(1 to "강남구",2 to "강동구", 3 to "강북구", 4 to "강서구", 5 to "관악구", 6 to "광진구", 7 to "구로구", 8 to "금천구", 9 to "노원구", 10 to "도봉구", 11 to "동대문구", 12 to "동작구", 13 to "마포구", 14 to "서대문구", 15  to "서초구", 16 to "성동구", 17 to "성북구", 18 to "송파구", 19 to "양천구", 20 to "영등포구", 21 to "용산구", 22 to "은평구", 23 to "종로구", 24 to "중구", 25 to "중랑구")
     val wdb=writableDatabase
     val rdb=readableDatabase
 
@@ -17,7 +21,12 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
 
         //DB 수정이 필요한 경우
         db.execSQL("DROP TABLE IF EXISTS '${users.TABLE_NAME}'")
-        db.execSQL("DROP TABLE IF EXISTS '${stores.TABLE_NAME}'")
+        for(gu in 1..25){
+            var sql= "DROP TABLE IF EXISTS '${zone[gu]}'"
+            db.execSQL(sql)
+            Log.i("create db","create stable ${zone[gu]}")
+        }
+        db.execSQL("DROP TABLE IF EXISTS '${deals.TABLE_NAME}'")
         onCreate(db)
     }
 
@@ -27,14 +36,34 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
         db.execSQL(SQL_CREATE_TABLE_USERS)
         Log.i("create db","create utable")
 
-        db.execSQL(SQL_CREATE_TABLE_STORES)
-        Log.i("create db","create stable")
+        for(gu in 1..25){
+            var sql= SQL_CREATE_TABLE_STORES_PRE+"${zone[gu]}"+ SQL_CREATE_TABLE_STORES_POST
+            db.execSQL(sql)
+            Log.i("create db","create stable ${zone[gu]}")
+        }
+
 
         db.execSQL(SQL_CREATE_TABLE_DEALS)
         Log.i("create db","create dtable")
 
     }
 
+    fun createTable(){
+
+        //zeroPayDB에 users 와 stores 테이블 생성
+        wdb.execSQL(SQL_CREATE_TABLE_USERS)
+        Log.i("create db","create utable")
+
+        for(gu in 1..25){
+            var sql= SQL_CREATE_TABLE_STORES_PRE+"${zone[gu]}"+ SQL_CREATE_TABLE_STORES_POST
+            wdb.execSQL(sql)
+            Log.i("create db","create stable ${zone[gu]}")
+        }
+
+
+        wdb.execSQL(SQL_CREATE_TABLE_DEALS)
+        Log.i("create db","create dtable")
+    }
 
     companion object {
         val DATABASE_VERSION=1
@@ -42,13 +71,23 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
 
         val SQL_CREATE_TABLE_USERS="CREATE TABLE ${users.TABLE_NAME}"+
                 "(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,"+ users.KEY_NAME+" TEXT,"+
-                users.KEY_BALANCE+" INTEGER,"+users.KEY_INCOME+" INTEGER );"
+                users.KEY_BALANCE+" INTEGER,"+users.KEY_INCOME+" TEXT );"
+
+        /*
         val SQL_CREATE_TABLE_STORES="CREATE TABLE ${stores.TABLE_NAME}"+
                 "(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,"+ stores.KEY_NAME+" TEXT,"+
                 stores.KEY_ADDR+" TEXT,"+stores.KEY_INFO+" TEXT);"
+         */
+
+
+        val SQL_CREATE_TABLE_STORES_PRE="CREATE TABLE ${stores.TABLE_NAME}"
+        val SQL_CREATE_TABLE_STORES_POST="(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,"+ stores.KEY_NAME+" TEXT,"+
+                stores.KEY_ADDR+" TEXT,"+stores.KEY_GU+" TEXT,"+stores.KEY_INFO+" TEXT);"
+
+
         val SQL_CREATE_TABLE_DEALS="CREATE TABLE ${deals.TABLE_NAME}"+
                 "(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                deals.KEY_DATE+" TEXT,"+ deals.KEY_NAME+" TEXT,"+deals.KEY_PRICE+" INTEGER);"
+                deals.KEY_DATE+" TEXT,"+ deals.KEY_NAME+" TEXT,"+deals.KEY_PRICE+" TEXT,"+deals.KEY_CATEGORY+" TEXT,"+deals.KEY_ISZERO+" INTEGER);"
 
 
     }
@@ -56,7 +95,6 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
     class users:BaseColumns{
         companion object {
             val TABLE_NAME="users"
-            val KEY_ID="u_id"
             val KEY_NAME="u_name"
             val KEY_BALANCE="u_balance"
             val KEY_INCOME="u_income"
@@ -68,6 +106,7 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
             val KEY_ID="s_id"
             val KEY_NAME="s_name"
             val KEY_ADDR="s_addr"
+            val KEY_GU="s_gu"
             val KEY_INFO="s_info"
         }
     }
@@ -79,6 +118,8 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
             val KEY_DATE="date"
             val KEY_NAME="store"
             val KEY_PRICE="price"
+            val KEY_CATEGORY="category"
+            val KEY_ISZERO="is_zeropay"
         }
     }
 
@@ -100,16 +141,19 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
     }
     fun insertStore(store: store){
 
+
         val stoAddr=store.addr
         if(stoAddr.length>0){
-            if(!findSameStore(stoAddr)){
+           // if(findSameStore(stoAddr,store.locality)){
+                if(findSameStore(stoAddr,store.locality)){
                 val values=ContentValues().apply {
                     put(stores.KEY_NAME,store.sname)
                     put(stores.KEY_ADDR,store.addr)
+                    put(stores.KEY_GU,store.locality)
                     put(stores.KEY_INFO,store.info)
                 }
-                val success=wdb.insert(stores.TABLE_NAME,null,values)
-                Log.i("InsertedStoreID: ","$success + storekeyID+ ${store.sid}")
+                val success=wdb.insert(stores.TABLE_NAME+store.locality,null,values)
+                Log.i("InsertedStoreID: ","${store.locality} +$success + storekeyID+ ${store.sid}")
 
             }
             else {
@@ -125,48 +169,50 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
             put(deals.KEY_DATE,deal.date)
             put(deals.KEY_NAME,deal.store)
             put(deals.KEY_PRICE,deal.price)
+            put(deals.KEY_CATEGORY,deal.category)
+            put(deals.KEY_ISZERO,deal.isZero)
         }
 
         val success=wdb.insert(deals.TABLE_NAME,null,values)
-        Log.i("InsertedDealID: ","$success InsertedDealID:  ${deal.did}")
+        Log.i("InsertedDealID: ","$success InsertedDealID:  ${BaseColumns._ID}")
 
     }
 
     //db 에서 삭제
-    fun deleteUser(uid:String){
-        val deleteUserQuery="DELETE FROM ${users.TABLE_NAME} WHERE ${users.KEY_ID} = ?"
-        val selectionArgs=arrayOf(uid)
-        wdb.rawQuery(deleteUserQuery,selectionArgs)
+    fun deleteUser(){
+        wdb.delete(users.TABLE_NAME,null,null)
     }
-    fun deleteStore(sid:String){
-        val selection=stores.KEY_ID+"=?"
+    fun deleteStore(sid:String,gu:String){   //gu 에 삭제할 store.locatlity  입력
+
+        val selection="${BaseColumns._ID} = ?"
         val selectionArgs=arrayOf(sid)
-        wdb.delete(stores.TABLE_NAME,selection,selectionArgs)
+        wdb.delete(stores.TABLE_NAME+gu,selection,selectionArgs)
     }
     fun deleteDeal(did: String){
-        val selection=deals.KEY_ID+"=?"
+        val selection="${BaseColumns._ID} = ?"
         val selectionArgs=arrayOf(did)
         wdb.delete(deals.TABLE_NAME,selection,selectionArgs)
     }
 
     //db 변경
-    fun updateUserIncome(uid:String,income:String){
+    fun updateUserIncome(income:String){
 
         val value=ContentValues()
         value.put(users.KEY_INCOME,income)
-        val selection="${users.KEY_ID} LIKE ?"
-        val selectionArgs=arrayOf(uid)
-        val count=wdb.update(users.TABLE_NAME,value,selection,selectionArgs)
+        //val selection="${users.KEY_ID} LIKE ?"
+        //val selectionArgs=arrayOf(uid)
+        //val count=wdb.update(users.TABLE_NAME,value,selection,selectionArgs)
+        val count=wdb.update(users.TABLE_NAME,value,null,null)
 
         Log.i("updateDB_user","$count")
     }
-    fun updateUserBalance(uid:String,balance:String){
+    fun updateUserBalance(balance:String){
         //balance 변경을 많이 쓸 것같아 따로.
         val value=ContentValues()
         value.put(users.KEY_BALANCE,balance)
-        val selection="${users.KEY_ID} LIKE ?"
-        val selectionArgs=arrayOf(uid)
-        val count=wdb.update(users.TABLE_NAME,value,selection,selectionArgs)
+        //val selection="${users.KEY_ID} LIKE ?"
+        //val selectionArgs=arrayOf(uid)
+        val count=wdb.update(users.TABLE_NAME,value,null,null)
 
         Log.i("updateDB_user","$count")
     }
@@ -176,12 +222,13 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
         val value=ContentValues().apply {
             put(stores.KEY_NAME,store.sname)
             put(stores.KEY_ADDR,store.addr)
+            put(stores.KEY_GU,store.locality)
             put(stores.KEY_INFO,store.info)
         }
 
-        val selection="${stores.KEY_ID} LIKE ?"
+        val selection="${BaseColumns._ID} LIKE ?"
         val selectionArgs=arrayOf(store.sid)
-        val count=wdb.update(stores.TABLE_NAME,value,selection,selectionArgs)
+        val count=wdb.update(stores.TABLE_NAME+store.locality,value,selection,selectionArgs)
 
         Log.i("update_db: ","$count")
 
@@ -192,9 +239,11 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
             put(deals.KEY_DATE,deal.date)
             put(deals.KEY_NAME,deal.store)
             put(deals.KEY_PRICE,deal.price)
+            put(deals.KEY_CATEGORY,deal.category)
+            put(deals.KEY_ISZERO,deal.isZero)
         }
 
-        val selection="${deals.KEY_ID} LIKE ?"
+        val selection="${BaseColumns._ID} LIKE ?"
         val selectionArgs=arrayOf(deal.did)
         val count=wdb.update(deals.TABLE_NAME,value,selection,selectionArgs)
 
@@ -202,85 +251,93 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
     }
 
     //db  조회
-    fun getUser(findID:String):user{
+    fun getUser(): user {
 
-            val selectAllQuery="SELECT * FROM ${users.TABLE_NAME} WHERE ${users.KEY_ID}=?"
-            val cursor=rdb.rawQuery(selectAllQuery,arrayOf(findID))
+        val selectAllQuery="SELECT * FROM ${users.TABLE_NAME}"
+        val cursor=rdb.rawQuery(selectAllQuery,null)
 
-            if(cursor!=null){
-                Log.i("finduser",cursor.columnCount.toString())
+        if(cursor!=null){
+            Log.i("finduser",cursor.columnCount.toString())
 
-                with(cursor){
-                    while(moveToNext()){
-
-                        var id=cursor.getString(cursor.getColumnIndex(com.example.zeropaytest.DBHelper.users.KEY_ID))
-
-                        if(findID==id){
-                            val name=cursor.getString(cursor.getColumnIndex(users.KEY_NAME))
-                            val income=cursor.getString(cursor.getColumnIndex(users.KEY_INCOME))
-                            val balance=cursor.getString(cursor.getColumnIndex(users.KEY_BALANCE))
-                            val result=user(id,name,balance.toInt(),income)
-                            return result
-                        }
-
-                    }
-                    Log.i("findUser","no user")
+            with(cursor){
+                while(moveToNext()){
+                    val name=cursor.getString(cursor.getColumnIndex(users.KEY_NAME))
+                    val income=cursor.getString(cursor.getColumnIndex(users.KEY_INCOME))
+                    val balance=cursor.getInt(cursor.getColumnIndex(users.KEY_BALANCE))
+                    val result=user(name,balance,income)
+                    return result
                 }
+
+                Log.i("findUser","no user")
             }
-            val fail=user("","",0,"")
-            return fail
         }
+        val fail=user("",0,"")
+        return fail
+    }
 
+    fun getDealsSize() { //gu: 찾으려는 store의 locality
+        val selectAllQuery = "SELECT * FROM ${deals.TABLE_NAME}"
+        val cursor = rdb.rawQuery(selectAllQuery, null)
+        val size=cursor.columnNames
 
+        for(i in size){
+            Log.i("dealsize", i.toString())
 
-
-
-
-    fun getUsers():ArrayList<user>{
-
-        val userList=ArrayList<user>()
-        val projection=arrayOf(users.KEY_ID,users.KEY_NAME,users.KEY_BALANCE,users.KEY_INCOME)
-        val cursor=rdb.query(users.TABLE_NAME,projection,null,null,null,null,null)
-        while(cursor.moveToNext()){
-            val uid=cursor.getString(cursor.getColumnIndex(users.KEY_ID))
-            val name=cursor.getString(cursor.getColumnIndex(users.KEY_NAME))
-            val income=cursor.getString(cursor.getColumnIndex(users.KEY_INCOME))
-            val balance=cursor.getString(cursor.getColumnIndex(users.KEY_BALANCE))
-            val user=user(uid,name,balance.toInt(),income)
-            userList.add(user)
         }
-        return userList
-    }
-    fun getStore(sid:String):store{
-           val store=getStores().get(sid.toInt()-1)
-        return store
-    }
-    fun getStores():ArrayList<store>{
+        Log.i("dealszie1",size.size.toString())
 
+
+    }
+
+
+    fun getStore() { //gu: 찾으려는 store의 locality
+        val str = stores.TABLE_NAME + "마포구"
+        val selectAllQuery = "SELECT * FROM $str"
+        val cursor = rdb.rawQuery(selectAllQuery, null)
+
+        val size=cursor.columnNames
+        for(i in size){
+            Log.i("storesize", i.toString())
+
+        }
+    }
+    fun getStores(gu:String):ArrayList<store>{
+            Log.i("getStore","getstore")
         val storeList=ArrayList<store>()
-        val projection=arrayOf(stores.KEY_ID,stores.KEY_NAME,stores.KEY_ADDR,stores.KEY_INFO)
-        val cursor=rdb.query(stores.TABLE_NAME,projection,null,null,null,null,null,null)
-        while(cursor.moveToNext()){
-            val sid=cursor.getString(cursor.getColumnIndex(stores.KEY_ID))
-            val name=cursor.getString(cursor.getColumnIndex(stores.KEY_NAME))
-            val addr=cursor.getString(cursor.getColumnIndex(stores.KEY_ADDR))
-            val info=cursor.getString(cursor.getColumnIndex(stores.KEY_INFO))
-            val store=store(sid,name,addr,info)
-            storeList.add(store)
+        val projection=arrayOf(BaseColumns._ID, DBHelper.stores.KEY_NAME, DBHelper.stores.KEY_ADDR,DBHelper.stores.KEY_GU,
+            DBHelper.stores.KEY_INFO)
+        val cursor=rdb.query(DBHelper.stores.TABLE_NAME+gu,projection,null,null,null,null,null,null)
+        if(cursor!=null) {
+            Log.i("getStore","cursor")
+            while (cursor.moveToNext()) {
+                val sid = cursor.getString(cursor.getColumnIndex(BaseColumns._ID))
+                val name = cursor.getString(cursor.getColumnIndex(DBHelper.stores.KEY_NAME))
+                val addr = cursor.getString(cursor.getColumnIndex(DBHelper.stores.KEY_ADDR))
+                val gu = cursor.getString(cursor.getColumnIndex(DBHelper.stores.KEY_GU))
+                val info = cursor.getString(cursor.getColumnIndex(DBHelper.stores.KEY_INFO))
+                val store = store(sid, name, addr, gu, info)
+                Log.i("getStore",store.sid)
+
+                storeList.add(store)
+            }
+        }
+        else{
+            Log.i("searchStores","no store")
         }
         return storeList
     }
-    fun findSameStore(newaddr:String):Boolean{
+    fun findSameStore(newaddr:String,gu: String):Boolean{
 
         val db=this.readableDatabase
-        val selectAllQuery="SELECT * FROM ${stores.TABLE_NAME} WHERE ${stores.KEY_ADDR} =?"
+        val selectAllQuery="SELECT * FROM ${stores.TABLE_NAME}$gu WHERE ${stores.KEY_ADDR} LIKE ?"
+        Log.i("findSameStore",stores.TABLE_NAME+gu)
         val selectionArgs=arrayOf(newaddr)
         val cursor=db.rawQuery(selectAllQuery,selectionArgs)
 
         if(cursor!=null)//같은 것 존재
-             return true
+            return true
         else
-             return false
+            return false
     }
     fun getDeal(did:String):deal{
 
@@ -290,16 +347,22 @@ class DBHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DAT
     fun getDeals():ArrayList<deal>{
 
         val dealList=ArrayList<deal>()
-        val projection=arrayOf(deals.KEY_ID,deals.KEY_NAME,deals.KEY_DATE,deals.KEY_PRICE)
-        val cursor=rdb.query(stores.TABLE_NAME,projection,null,null,null,null,null,null)
-        while(cursor.moveToNext()){
-            val did=cursor.getString(cursor.getColumnIndex(deals.KEY_ID))
-            val sname=cursor.getString(cursor.getColumnIndex(deals.KEY_NAME))
-            val date=cursor.getString(cursor.getColumnIndex(deals.KEY_DATE))
-            val price=cursor.getString(cursor.getColumnIndex(deals.KEY_PRICE))
+        val projection=arrayOf(BaseColumns._ID,deals.KEY_NAME,deals.KEY_DATE,deals.KEY_PRICE,deals.KEY_CATEGORY,deals.KEY_ISZERO)
+        val cursor=rdb.query(deals.TABLE_NAME,projection,null,null,null,null,null,null)
+        if(cursor!=null) {
+            while (cursor.moveToNext()) {
+                val did = cursor.getString(cursor.getColumnIndex(BaseColumns._ID))
+                val sname = cursor.getString(cursor.getColumnIndex(deals.KEY_NAME))
+                val date = cursor.getString(cursor.getColumnIndex(deals.KEY_DATE))
+                val price = cursor.getString(cursor.getColumnIndex(deals.KEY_PRICE))
+                val category = cursor.getString(cursor.getColumnIndex(deals.KEY_CATEGORY))
+                val isZero = cursor.getInt(cursor.getColumnIndex(deals.KEY_ISZERO))
 
-            val deal=deal(did,sname,date,price)
-            dealList.add(deal)
+                val deal = deal(did, sname, date, price, category, isZero)
+                dealList.add(deal)
+            }
+        }else{
+            Log.i("searchdeal","nodeal")
         }
         return dealList
     }
